eef49a6cd78e54903dfdbc1c0d5b3fae8ae697cd
ArmanNavodia <armannavodia2002@gmail.com>
2025-05-12 16:42:01 +0530

    integrated packer with terraform

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "latest_packer_ami" {
+    depends_on = [ null_resource.build_ami ]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["my-custom-ami-*"]  # match your AMI name pattern
+  }
+
+  owners = ["self"]  # only your account's AMIs
+}
+
+resource "aws_instance" "nginx_server" {
+  ami                         = data.aws_ami.latest_packer_ami.id
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-02f995dae74f270c8"
+  associate_public_ip_address = true
+  depends_on                  = [null_resource.build_ami]
+  tags = {
+    "name" = "nginx_server"
+  }
+}
+resource "null_resource" "build_ami" {
+  provisioner "local-exec" {
+    command = "packer build ..\\packer\\"
+  }
+}
